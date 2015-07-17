f60b4848a6b038922449d33c9fd5096c3afd1673
Minghan.liu <lmh5257@live.cn>
2015-07-17 13:27:12 +0800

    First commit

--- run.sh
+++ run.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-echo "Hi~~~"
+echo "Hi~~~ eMQTT Starting"
 
 
-if [ ! -f /.root_passwd_set ]; then
-	/home/docker/code/set_root_pwd.sh
-fi
-
-MODULE=${MODULE:-website}
-echo "finding django project (module: ${MODULE})"
-sed -i "s#module=website.wsgi:application#module=${MODULE}.wsgi:application#g" /home/docker/code/uwsgi.ini
-
-if [ ! -f "/home/docker/code/app/manage.py" ]
-then
-	echo "creating basic django project (module: ${MODULE})"
-	django-admin.py startproject ${MODULE} /home/docker/code/app/
-fi
-
-/usr/bin/supervisord
-exec /usr/sbin/sshd -D
+./home/docker/code/emqttd/bin/emqttd console

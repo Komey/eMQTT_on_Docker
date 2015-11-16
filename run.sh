c980a9ebdc866141c415cf19b29aed998976b9b0
Komey <lmh5257@live.cn>
2015-11-16 15:55:09 +0800

    Fix bug

--- run.sh
+++ run.sh
@@ -2,34 +2,29 @@
 
 if [ -f /.root_passwd_set ]; then
 	echo "Root password already set!"
-	exit 0
+else
+	PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
+	_word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
+	echo "=> Setting a ${_word} password to the root user"
+	echo "root:$PASS" | chpasswd
+
+	echo "=> Done!"
+	touch /.root_passwd_set
+
+	echo "========================================================================"
+	echo "You can now connect to this container via SSH using:"
+	echo ""
+	echo " ssh -p <port> root@<host>"
+	echo " and enter the root password '$PASS' "
+	echo ""
+	echo "remember to change the above password !"
+	echo "========================================================================"
 fi
-
-PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
-_word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
-echo "=> Setting a ${_word} password to the root user"
-echo "root:$PASS" | chpasswd
-
-echo "=> Done!"
-touch /.root_passwd_set
-
-echo "========================================================================"
-echo "You can now connect to this container via SSH using:"
-echo ""
-echo " ssh -p <port> root@<host>"
-echo " and enter the root password '$PASS' "
-echo ""
-echo "remember to change the above password !"
-echo "========================================================================"
-
 
 echo "eMQTT Broker Starting"
 
 
+./home/file/emqttd/bin/emqttd console
 
-./home/file/emqttd/bin/emqttd start
 
-
-echo "Done."
-/usr/bin/supervisord
 exec /usr/sbin/sshd -D
